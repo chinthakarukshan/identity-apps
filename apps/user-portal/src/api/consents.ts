43c33548435dc48bdba4c85431cc15fbc449baa1
Yasin Miran <wytm97@protonmail.com>
2020-12-09 10:23:41 +0530

    fixed linting issue

--- apps/user-portal/src/api/consents.ts
+++ apps/user-portal/src/api/consents.ts
@@ -111,11 +111,11 @@ export const fetchConsentReceipt = (receiptId: string): Promise<any> => {
  * - {@link offset} will defaults to 0 if not provided. If the offset is zero
  * server will start searching the records starting from zero.
  *
- * @param {number | 0} limit Number of search results
- * @param {number | 0} offset Start index of the search
+ * @param {number} limit Number of search results
+ * @param {number} offset Start index of the search
  * @return {Promise<PurposeModelPartial[]>} response data
  */
-export const fetchAllPurposes = async (limit: number = 0, offset: number = 0): Promise<PurposeModelPartial[]> => {
+export const fetchAllPurposes = async (limit = 0, offset = 0): Promise<PurposeModelPartial[]> => {
 
     const requestConfig: AxiosRequestConfig = {
         headers: {
